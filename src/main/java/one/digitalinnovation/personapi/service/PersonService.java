f0f9183805e35f05b70d06e2bddaa8e9459703af
atomita <atomita@uolinc.com>
2022-01-29 22:12:37 -0300

    personDTO e personMapper

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -1,25 +1,39 @@
 package one.digitalinnovation.personapi.service;
 
 import one.digitalinnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personapi.dto.resquest.PersonDTO;
 import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.mapper.PersonMapper;
 import one.digitalinnovation.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class PersonService {
 
     private PersonRepository personRepository;
 
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
+
     @Autowired
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
-    public MessageResponseDTO createPerson(Person person){
-        Person savedPerson = personRepository.save(person);
+    public MessageResponseDTO createPerson(PersonDTO personDTO){
+
+        Person personToSave = personMapper.toModel(personDTO);
+
+//PersonMapper substitui o codigo abaixo, fazendo o mapeamento do personDTO para person
+//        Person personToSalve = Person.builder()
+//                .firstName(personDTO.getFirstName())
+//                .lastName(personDTO.getLastName())
+//                .birthDate(personDTO.getBirthDate())
+//                .phones(personDTO.getPhones())
+//                .build();
+
+
+        Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO
                 .builder()
                 .message("Created person with ID " + savedPerson.getId())
